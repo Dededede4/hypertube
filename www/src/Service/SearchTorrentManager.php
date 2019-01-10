9ca25bdf0906a0c3ff5052c692a23f8483cc691b
Émile PRÉVOT <prevotmorvan@yahoo.fr>
2019-01-10 20:05:15 +0100

    Add draft for search with archive.org

--- www/src/Service/SearchTorrentManager.php
+++ www/src/Service/SearchTorrentManager.php
@@ -19,6 +19,11 @@ class SearchTorrentManager
 
     public function search($query)
     {
+        return array_merge($this->searchArchiveOrg($query), $this->searchLegitTorrent($query));
+    }
+
+    public function searchLegitTorrent($query)
+    {
         $url = 'http://www.legittorrents.info/index.php?search='.$query.'&page=torrents&category=1&active=1';
 
         $html = file_get_contents($url);
@@ -57,6 +62,16 @@ class SearchTorrentManager
                 }   
             }
         }
+        return $results;
+    }
+
+    public function searchArchiveOrg($query)
+    {
+        $url = 'https://archive.org/advancedsearch.php?q=mediatype%3Atexts+'.$query.'&fl%5B%5D=avg_rating&fl%5B%5D=backup_location&fl%5B%5D=btih&fl%5B%5D=call_number&fl%5B%5D=collection&fl%5B%5D=contributor&fl%5B%5D=coverage&fl%5B%5D=creator&fl%5B%5D=date&fl%5B%5D=description&fl%5B%5D=downloads&fl%5B%5D=external-identifier&fl%5B%5D=foldoutcount&fl%5B%5D=format&fl%5B%5D=genre&fl%5B%5D=headerImage&fl%5B%5D=identifier&fl%5B%5D=imagecount&fl%5B%5D=indexflag&fl%5B%5D=item_size&fl%5B%5D=language&fl%5B%5D=licenseurl&fl%5B%5D=mediatype&fl%5B%5D=members&fl%5B%5D=month&fl%5B%5D=name&fl%5B%5D=noindex&fl%5B%5D=num_reviews&fl%5B%5D=oai_updatedate&fl%5B%5D=publicdate&fl%5B%5D=publisher&fl%5B%5D=related-external-id&fl%5B%5D=reviewdate&fl%5B%5D=rights&fl%5B%5D=scanningcentre&fl%5B%5D=source&fl%5B%5D=stripped_tags&fl%5B%5D=subject&fl%5B%5D=title&fl%5B%5D=type&fl%5B%5D=volume&fl%5B%5D=week&fl%5B%5D=year&sort%5B%5D=titleSorter+asc&sort%5B%5D=&sort%5B%5D=&rows=10&page=1&output=json&save=yes#raw';
+        $results = json_decode(file_get_contents($url), true);
+        
+        // Check if result is empty
+        $results = $results['response']['docs'];
         return $results;
     }
 }
